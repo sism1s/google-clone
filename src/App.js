349b691a84bad0bf2b947ef0840588997344050e
sism1s <marko.lazic880@gmail.com>
2021-04-26 15:51:34 +0100

    Created Gmail header

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import "./App.css";
 import Home from "./pages/Home";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import SearchPage from "./pages/SearchPage";
+import Gmail from "./pages/Gmail";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Switch>
+          <Route path="/gmail">
+            <Gmail />
+          </Route>
           <Route path="/search">
             <SearchPage />
           </Route>
---
+++ src/pages/Gmail.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "../components/Header";
+import "./Gmail.css";
+
+function Gmail() {
+  return (
+    <div className="gmail">
+      <Header />
+    </div>
+  );
+}
+
+export default Gmail;
