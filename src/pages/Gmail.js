fef84a22a68f73a422f10e9843608f55f199b672
sism1s <marko.lazic880@gmail.com>
2021-04-27 16:07:36 +0100

    created sidebar

--- src/pages/Gmail.js
+++ src/pages/Gmail.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "../components/Header";
+import Sidebar from "../components/Sidebar";
 import "./Gmail.css";
 
 function Gmail() {
   return (
     <div className="gmail">
       <Header />
+      <Sidebar />
     </div>
   );
 }
