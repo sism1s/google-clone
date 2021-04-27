1cc897d30f418cd8c541f27592cf025773537776
sism1s <marko.lazic880@gmail.com>
2021-04-27 17:39:04 +0100

    created sticky sections

--- src/pages/Gmail.js
+++ src/pages/Gmail.js
@@ -2,13 +2,28 @@ import React from "react";
 import Header from "../components/Header";
 import Sidebar from "../components/Sidebar";
 import "./Gmail.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Mail from "./Mail";
+import EmailList from "./EmailList";
 
 function Gmail() {
   return (
-    <div className="gmail">
-      <Header />
-      <Sidebar />
-    </div>
+    <Router>
+      <div className="gmail">
+        <Header />
+        <div className="gmail__body">
+          <Sidebar />
+          <Switch>
+            <Route path="/mail">
+              <Mail />
+            </Route>
+            <Route path="/gmail">
+              <EmailList />
+            </Route>
+          </Switch>
+        </div>
+      </div>
+    </Router>
   );
 }
 
